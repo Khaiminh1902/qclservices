6d5c67e611092b252028cc9c233c36a6b66a0be7
Khaiminh1902 <khaiminhnguyen1902@gmail.com>
2025-03-02 16:53:33 +0700

    02/03/2025

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -3,7 +3,6 @@ import Slider from "../components/Slider";
 import Footer from "../components/Footer";
 import FixedButton from "../components/FixedButton";
 import Letter from "../components/Letter";
-import Navbar_en from "../components/Navbar_en";
 
 const Home_vn = () => {
   return (
--- app/vn/page.tsx
+++ app/vn/page.tsx
@@ -3,8 +3,6 @@ import Slider from "../components/Slider";
 import Footer from "../components/Footer";
 import FixedButton from "../components/FixedButton";
 import Letter_vn from "../components/Letter_vn";
-import Navbar_vn from "../components/Navbar_vn";
-
 const Home_vn = () => {
   return (
     <div>
